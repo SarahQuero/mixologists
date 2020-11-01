4681729a8e8c25921740d15364bfcfeceb2e0d4b
Sarah Q <sarahquero@gmail.com>
2020-11-01 16:53:53 +0100

    creation of FicheCocktail

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,26 @@ import Lab from "./components/Lab";
 import World from "./components/World";
 import Box from "./components/Box";
 import Contact from "./components/Contact";
+import FicheCocktail from "./components/FicheCocktail";
 import "./App.scss";
+
+const piscoSour = {
+  idDrink: "13214",
+  strDrink: "Pisco Sour",
+  strCategory: "Cocktail",
+  strAlcoholic: "Alcoholic",
+  strGlass: "Cocktail glass",
+  strInstructions:
+    "Vigorously shake and strain contents in a cocktail shaker with ice cubes, then pour into glass and garnish with bitters.[1]",
+  strIngredient1: "Pisco",
+  strIngredient2: "Lemon juice",
+  strIngredient3: "Sugar",
+  strIngredient4: "Ice",
+  strMeasure1: "2 oz ",
+  strMeasure2: "1 oz ",
+  strMeasure3: "1-2 tblsp ",
+  strMeasure4: null,
+};
 
 function App() {
   return (
@@ -19,6 +38,13 @@ function App() {
         <Route path="/Box" component={Box} />
         <Route path="/Contact" component={Contact} />
       </Switch>
+
+      <FicheCocktail
+        cocktailName={piscoSour.strDrink}
+        instructions={piscoSour.strInstructions}
+        ingredient1={piscoSour.strIngredient1}
+        ingredient2={piscoSour.strIngredient2}
+      />
     </div>
   );
 }
