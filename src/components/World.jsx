698b6f8ffdac3e647de2dcf4c4a3ec01f7f310bc
Jérémy Cellier <j.cellier28@gmail.com>
2020-11-13 10:58:13 +0100

    init globe

--- src/components/World.jsx
+++ src/components/World.jsx
@@ -1,7 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Style/World.scss";
+import ReactGlobe from "react-globe";
+
+// import defaultMarkers from "./markers";
+
+function markerTooltipRenderer(marker) {
+  return `CITY: ${marker.city} (Value: ${marker.value})`;
+}
+
+const options = {
+  markerTooltipRenderer,
+};
 
 function World() {
-  return <div className="margin">World</div>;
+  const [markers] = useState([]);
+  const [event, setEvent] = useState(null);
+  const [details, setDetails] = useState(null);
+  function onClickMarker(marker, markerObject, events) {
+    setEvent({
+      type: "CLICK",
+      marker,
+      markerObjectID: markerObject.uuid,
+      pointerEventPosition: { x: events.clientX, y: events.clientY },
+    });
+    setDetails(markerTooltipRenderer(marker));
+  }
+  function onDefocus(previousFocus) {
+    setEvent({
+      type: "DEFOCUS",
+      previousFocus,
+    });
+    setDetails(null);
+  }
+
+  return (
+    <div>
+      {details && (
+        <div
+          style={{
+            background: "white",
+            position: "absolute",
+            fontSize: 20,
+            bottom: 0,
+            right: 0,
+            padding: 12,
+          }}
+        >
+          <p>{details}</p>
+          <p>
+            EVENT: type={event.type}, position=
+            {JSON.stringify(event.pointerEventPosition)})
+          </p>
+        </div>
+      )}
+      <ReactGlobe
+        height="100vh"
+        markers={markers}
+        options={options}
+        width="100vw"
+        onClickMarker={onClickMarker}
+        onDefocus={onDefocus}
+      />
+    </div>
+  );
 }
+
 export default World;
