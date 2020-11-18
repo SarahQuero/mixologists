fc374bec7bcfc4a818b6fc4cf11293177be35985
Jérémy Cellier <j.cellier28@gmail.com>
2020-11-18 10:51:59 +0100

    Add card on globe

--- src/components/World.jsx
+++ src/components/World.jsx
@@ -1,16 +1,8 @@
 import React, { useState } from "react";
 import "./Style/World.scss";
 import ReactGlobe from "react-globe";
-
+import FicheCocktail from "./FicheCocktail";
 // import defaultMarkers from "./markers";
-
-function markerTooltipRenderer() {
-  return "Click for more information";
-}
-
-const options = {
-  markerTooltipRenderer,
-};
 
 function World() {
   const markers = [
@@ -20,6 +12,7 @@ function World() {
       color: "red",
       coordinates: [1.3521, 103.8198],
       value: 50,
+      cock: "margarita",
     },
     {
       id: "marker2",
@@ -27,6 +20,7 @@ function World() {
       color: "blue",
       coordinates: [40.73061, -73.935242],
       value: 25,
+      cock: "Mother's Milk",
     },
     {
       id: "marker3",
@@ -34,6 +28,7 @@ function World() {
       color: "orange",
       coordinates: [37.773972, -122.431297],
       value: 35,
+      cock: "Chocolate Black Russian",
     },
     {
       id: "marker4",
@@ -41,6 +36,7 @@ function World() {
       color: "gold",
       coordinates: [39.9042, 116.4074],
       value: 135,
+      cock: "Black & Tan",
     },
     {
       id: "marker5",
@@ -48,6 +44,7 @@ function World() {
       color: "green",
       coordinates: [51.5074, 0.1278],
       value: 80,
+      cock: "Adam & Eve",
     },
     {
       id: "marker6",
@@ -55,51 +52,65 @@ function World() {
       color: "gold",
       coordinates: [29.7604, -95.3698],
       value: 54,
+      cock: "New York Lemonade",
     },
   ];
-  const [event, setEvent] = useState(null);
-  const [details, setDetails] = useState(null);
-  function onClickMarker(marker, markerObject, events) {
-    setEvent({
-      type: "CLICK",
-      marker,
-      markerObjectID: markerObject.uuid,
-      pointerEventPosition: { x: events.clientX, y: events.clientY },
-    });
-    setDetails(markerTooltipRenderer(marker));
+  const [cock, setCock] = useState([]);
+  const [cardVisible, setCard] = useState(false);
+  const getAllIngre = (array) => {
+    const newAray = [];
+    for (let i = 1; i < 16; i += 1) {
+      if (
+        array[`strIngredient${i}`] === null ||
+        array[`strIngredient${i}`] === ""
+      ) {
+        break;
+      }
+      newAray.push({
+        name: array[`strIngredient${i}`],
+        good: true,
+      });
+    }
+    return newAray;
+  };
+
+  function markerTooltipRenderer() {
+    return !cardVisible ? "Click for more information" : "";
   }
-  function onDefocus(previousFocus) {
-    setEvent({
-      type: "DEFOCUS",
-      previousFocus,
-    });
-    setDetails(null);
+
+  function onClickMarker(marker) {
+    fetch(
+      `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${marker.cock}`
+    )
+      .then((data) => data.json())
+      .then((cocktail) => {
+        setCock(cocktail.drinks[0]);
+        setCard(true);
+      });
+  }
+  function onDefocus() {
+    setCard(false);
+    setCock([]);
   }
 
   return (
     <div>
-      {details && (
+      {cardVisible && (
         <div
           style={{
             background: "white",
-            position: "absolute",
             fontSize: 20,
             bottom: 0,
-            right: 0,
             padding: 12,
           }}
         >
-          <p>{details}</p>
-          <p>
-            EVENT: type={event.type}, position=
-            {JSON.stringify(event.pointerEventPosition)})
-          </p>
+          <FicheCocktail cocktail={cock} infoIng={getAllIngre(cock)} />
         </div>
       )}
       <ReactGlobe
         height="100vh"
         markers={markers}
-        options={options}
+        options={{ markerTooltipRenderer }}
         width="100vw"
         onClickMarker={onClickMarker}
         onDefocus={onDefocus}
