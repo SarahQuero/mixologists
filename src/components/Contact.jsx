b7a557309ce3848ce426ba05fd583dcdae06815c
Sarah Q <sarahquero@gmail.com>
2020-11-25 14:40:06 +0100

    creation of the contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Style/Contact.scss";
+import contactImage from "../../public/assets/contact-image.jpg";
 
-function Contact() {
-  return <div className="margin">Contact</div>;
-}
+const Contact = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [favCock, setFavCock] = useState("");
+  const [comment, setComment] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div
+      className="contact-form margin"
+      style={{
+        backgroundImage: `url(${contactImage})`,
+      }}
+    >
+      <form className="main-form" onSubmit={handleSubmit}>
+        <div className="fields-form">
+          <h1>Contact Us</h1>
+          <input
+            type="text"
+            value={firstName}
+            placeholder="First Name"
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          <input
+            type="text"
+            value={lastName}
+            placeholder="Last Name"
+            onChange={(e) => setLastName(e.target.value)}
+          />
+          <input
+            type="email"
+            value={email}
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="text"
+            value={favCock}
+            placeholder="My favourite cocktail"
+            onChange={(e) => setFavCock(e.target.value)}
+          />
+          <textarea
+            type="text"
+            value={comment}
+            placeholder="My Comments"
+            onChange={(e) => setComment(e.target.value)}
+          />
+          <button className="submit-form" type="submit">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 export default Contact;
