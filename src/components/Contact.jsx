8a3533f32b71c18b8a81e5277f59c52d104134f3
Sarah Q <sarahquero@gmail.com>
2020-11-26 10:06:45 +0100

    one last minor change

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -28,93 +28,95 @@ const Contact = () => {
         backgroundImage: `url(${contactImage})`,
       }}
     >
-      <form className="main-form" onSubmit={handleSubmit}>
-        <div className="fields-form">
-          <h1>Contact Us</h1>
-          <input
-            type="text"
-            className="general-input"
-            value={firstName}
-            placeholder="First Name"
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-          <input
-            type="text"
-            className="general-input"
-            value={lastName}
-            placeholder="Last Name"
-            onChange={(e) => setLastName(e.target.value)}
-          />
-          <input
-            type="email"
-            className="general-input"
-            value={email}
-            placeholder="Email"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <div className="favourite-cock-choice">
-            <div className="radio-buttom-field">
-              <p>Please vote for your favourite Cocktail:</p>
+      <div className="contact-form-content">
+        <form className="main-form" onSubmit={handleSubmit}>
+          <div className="fields-form">
+            <h1>Contact Us</h1>
+            <input
+              type="text"
+              className="general-input"
+              value={firstName}
+              placeholder="First Name"
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+            <input
+              type="text"
+              className="general-input"
+              value={lastName}
+              placeholder="Last Name"
+              onChange={(e) => setLastName(e.target.value)}
+            />
+            <input
+              type="email"
+              className="general-input"
+              value={email}
+              placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <div className="favourite-cock-choice">
+              <div className="radio-buttom-field">
+                <p>Please vote for your favourite Cocktail:</p>
+              </div>
+              <div className="radio-buttom-field">
+                <label htmlFor="pisco">
+                  <input
+                    type="radio"
+                    className="radio-buttom"
+                    checked={radio === "Pisco sour"}
+                    value="Pisco sour"
+                    id="pisco"
+                    onChange={(e) => {
+                      setRadio(e.target.value);
+                    }}
+                  />
+                  Pisco sour
+                </label>
+              </div>
+              <div className="radio-buttom-field">
+                <label htmlFor="manhattan">
+                  <input
+                    type="radio"
+                    className="radio-buttom"
+                    checked={radio === "Manhattan"}
+                    value="Manhattan"
+                    id="manhattan"
+                    onChange={(e) => {
+                      setRadio(e.target.value);
+                    }}
+                  />
+                  Manhattan
+                </label>
+              </div>
+              <div className="radio-buttom-field">
+                <label htmlFor="caipirinha">
+                  <input
+                    type="radio"
+                    className="radio-buttom"
+                    checked={radio === "Caipirinha"}
+                    value="Caipirinha"
+                    id="caipirinha"
+                    onChange={(e) => {
+                      setRadio(e.target.value);
+                    }}
+                  />
+                  Caipirinha
+                </label>
+              </div>
             </div>
-            <div className="radio-buttom-field">
-              <label htmlFor="pisco">
-                <input
-                  type="radio"
-                  className="radio-buttom"
-                  checked={radio === "Pisco sour"}
-                  value="Pisco sour"
-                  id="pisco"
-                  onChange={(e) => {
-                    setRadio(e.target.value);
-                  }}
-                />
-                Pisco sour
-              </label>
-            </div>
-            <div className="radio-buttom-field">
-              <label htmlFor="manhattan">
-                <input
-                  type="radio"
-                  className="radio-buttom"
-                  checked={radio === "Manhattan"}
-                  value="Manhattan"
-                  id="manhattan"
-                  onChange={(e) => {
-                    setRadio(e.target.value);
-                  }}
-                />
-                Manhattan
-              </label>
-            </div>
-            <div className="radio-buttom-field">
-              <label htmlFor="caipirinha">
-                <input
-                  type="radio"
-                  className="radio-buttom"
-                  checked={radio === "Caipirinha"}
-                  value="Caipirinha"
-                  id="caipirinha"
-                  onChange={(e) => {
-                    setRadio(e.target.value);
-                  }}
-                />
-                Caipirinha
-              </label>
+            <textarea
+              type="text"
+              value={comment}
+              placeholder="&darr; Tell us what you think of our website and how we could improve your experience &darr; "
+              onChange={(e) => setComment(e.target.value)}
+            />
+            <div>
+              <button className="submit-form" type="submit">
+                Submit
+              </button>
             </div>
           </div>
-          <textarea
-            type="text"
-            value={comment}
-            placeholder="&darr; Tell us what you think of our website and how we could improve your experience &darr; "
-            onChange={(e) => setComment(e.target.value)}
-          />
-          <div>
-            <button className="submit-form" type="submit">
-              Submit
-            </button>
-          </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   );
 };
